1784617e73c907a4815144ca00ba487a4b0869b5
Antony-evm <60288950+Antony-evm@users.noreply.github.com>
2021-01-20 11:17:18 +0000

    uploaded first version of customer/bike/map

---
+++ Bike.py
@@ -0,0 +1,59 @@
+from Map import *
+
+
+class Bike():
+
+	def __init__(self,defective,Id,location):
+		self.Id = Id
+		self.location = location
+		self.defective = defective
+		#map.set_state(location,1)
+
+	def is_defective(self):
+		return self.defective
+
+	def set_defective(self):
+		self.defective = not self.defective
+		return self.defective
+
+	def get_location(self):
+		return self.location
+
+	def set_location(self,location):
+		self.location = location
+		conn = sqlite3.connect('TEAM_PJT.db')
+		c = conn.cursor()
+		c.execute("UPDATE bike set location_row =:location_row, location_col=:location_col where id=:Id",
+					{'location_row':location[0],'location_col':location[1],'Id':self.Id})
+		conn.commit()
+		conn.close()
+
+	def get_id(self):
+		return self.id
+
+	def move(self,direction,map):
+		location = self.get_location()
+		og_val = map.get_square_val(location)
+		if direction=='up':
+			map.set_state(location,og_val-1)
+			location[0]-=1
+			self.set_location(location)
+			map.set_state(location,map.get_square_val(location)+1)
+
+		elif direction=='down':
+			map.set_state(location,og_val-1)
+			location[0]+=1
+			self.set_location(location)
+			map.set_state(location,map.get_square_val(location)+1)
+
+		elif direction == 'left':
+			map.set_state(location,og_val-1)
+			location[1]-=1
+			self.set_location(location)
+			map.set_state(location,map.get_square_val(location)+1)
+
+		else:
+			map.set_state(location,og_val-1)
+			location[1]+=1
+			self.set_location(location)
+			map.set_state(location,map.get_square_val(location)+1)
---
+++ insert_data.py
@@ -0,0 +1,48 @@
+import sqlite3
+
+db_name = 'TEAM_PJT.db'
+
+try:
+	conn = sqlite3.connect(db_name)
+except sqlite3.Error as er:
+	print(er)
+
+c = conn.cursor()
+
+
+
+c.execute("INSERT INTO customer (name,password,wallet,location_row,location_col) VALUES('{}','{}',{},{},{})".format("Tony","1234",132.5,10,11))
+c.execute("INSERT INTO customer (name,password,wallet,location_row,location_col) VALUES('{}','{}',{},{},{})".format("Jichen","12345",232.5,0,1))
+c.execute("INSERT INTO customer (name,password,wallet,location_row,location_col) VALUES('{}','{}',{},{},{})".format("Shihao","123456",172.5,8,3))
+c.execute("INSERT INTO customer (name,password,wallet,location_row,location_col) VALUES('{}','{}',{},{},{})".format("Yuan","1234567",188.6,4,12))
+
+
+c.execute("INSERT INTO bike (defective,location_row,location_col) VALUES({},{},{})".format(0,11,15))
+c.execute("INSERT INTO bike (defective,location_row,location_col) VALUES({},{},{})".format(0,12,16))
+c.execute("INSERT INTO bike (defective,location_row,location_col) VALUES({},{},{})".format(0,13,10))
+c.execute("INSERT INTO bike (defective,location_row,location_col) VALUES({},{},{})".format(0,7,5))
+c.execute("INSERT INTO bike (defective,location_row,location_col) VALUES({},{},{})".format(0,4,17))
+c.execute("INSERT INTO bike (defective,location_row,location_col) VALUES({},{},{})".format(0,10,19))
+c.execute("INSERT INTO bike (defective,location_row,location_col) VALUES({},{},{})".format(0,19,18))
+c.execute("INSERT INTO bike (defective,location_row,location_col) VALUES({},{},{})".format(0,17,16))
+c.execute("INSERT INTO bike (defective,location_row,location_col) VALUES({},{},{})".format(0,14,12))
+c.execute("INSERT INTO bike (defective,location_row,location_col) VALUES({},{},{})".format(0,1,6))
+
+
+
+
+conn.commit()
+
+c.execute("SELECT * FROM customer")
+rows=c.fetchall()
+for i in rows:
+	print(i)
+
+
+
+c.execute("SELECT * FROM bike")
+rows=c.fetchall()
+for i in rows:
+	print(i)
+
+conn.close()
