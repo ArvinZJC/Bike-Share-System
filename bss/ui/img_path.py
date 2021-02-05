da7d294e2d524daa8ab6acb8b090f51a75189898
ArvinZJC <tomzjc@qq.com>
2021-02-05 17:20:56 +0000

    Add an image file location helper to solve the issue of failing to locate an image when script files in various sub-packages are run.

---
+++ bss/ui/img_path.py
@@ -0,0 +1,39 @@
+'''
+Description: an image file location helper
+Version: 1.0.0.20210205
+Author: Arvin Zhao
+Date: 2021-02-05 14:04:27
+Last Editors: Arvin Zhao
+LastEditTime: 2021-02-05 14:05:23
+'''
+
+import os
+
+from bss.conf import attrs
+
+
+def get_img_path(img_filename: str) -> str:
+    '''
+    Get the relative path of an image based on the running location of a script file.
+
+    Parameters
+    ----------
+    img_filename : the filename of the image
+
+    Returns
+    -------
+    db_path : the suitable relative path of the database file
+    '''
+
+    basename = os.path.basename(os.path.abspath('.'))  # Get the basename of the script file that is run.
+
+    if basename == attrs.UI_BASENAME:
+        return os.path.join(attrs.UI_IMG_BASENAME, img_filename)
+    elif basename == attrs.ROOT_BASENAME:
+        return os.path.join(attrs.UI_BASENAME, attrs.UI_IMG_BASENAME, img_filename)
+    else:
+        return os.path.join('..', attrs.UI_BASENAME, attrs.UI_IMG_BASENAME, img_filename)
+
+
+if __name__ == '__main__':
+    print(get_img_path(attrs.APP_BANNER_FILENAME))
